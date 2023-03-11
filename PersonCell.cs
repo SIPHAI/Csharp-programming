d30715656db7bdaa39de7eccc4731ebbf484231c
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2023-03-11 22:10:11 +0700

    Add files via upload

---
+++ PersonCell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkListCLI { 
+
+    internal class PersonCell
+    {
+    public string Name;
+    public PersonCell Next;
+    public PersonCell() { }
+
+    public PersonCell(string name, PersonCell next)
+    {
+        Name = name;
+        Next = next;
+    }
+    public void InsertAfter(string newName)
+    {
+        PersonCell newCell = new PersonCell();
+        newCell.Name = newName;
+        newCell.Next = this.Next;
+        this.Next = newCell;
+    }
+        public PersonCell FindCell(string name)
+        {
+            {
+                for (PersonCell cell = this; ;cell = cell.Next)
+                {
+                    if (cell == null) return null;
+                    if (cell.Name == name) return cell;
+                }
+                   
+            }
+            public void InsertAfter(string afterName, string newName)
+            {
+                PersonCell afterCell = FindCell(afterName);
+                if (afterCell == null)
+                {
+                    throw new KeyNotFoundException("Item" + afterName + "not found in List");
+                }
+                afterCell.InsertAfter(newName);
+            }
+            
+        }
+    }
+}
