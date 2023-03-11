e5912726dfce6abbc3758f5c8cff5522942e3b54
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2023-03-11 22:09:18 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RecursiveFibonacci
+namespace SinglyLinkListCLI
 {
     internal class Program
     {
-        static int recursiveFibonacci(int N)
-        { // Your Recursive Function coding goes here
-
-            if (N == 0)
-            {
-                return 0;
-            }
-            if (N == 1)
-            {
-                return 1;
-            }
-            return recursiveFibonacci(N - 1) + recursiveFibonacci(N - 2);
-
-        }
-
         static void Main(string[] args)
         {
-            int N;
-            do
-            {
-                Console.WriteLine("This program display a list of Fibonacci numbers up to");
-                Console.Write("the sequence number you enter (maximum value 20): ");
-                N = int.Parse(Console.ReadLine());
-            } while (N <= 0 || N > 20);
+            PersonCell TopCell = new PersonCell("<sentinel>", null);
 
-
-            Console.WriteLine("The Fibonacci Sequence:");
             int x = 0;
-            while (x < N)
+            while (x != 5)
             {
-                Console.WriteLine("The {0,2}th Fibonacci number is {1,4}", x + 1, recursiveFibonacci(x));
-                x++;
+                Console.Clear();
+                Console.WriteLine("Enter 0 to display the linked list.");
+                Console.WriteLine("Enter 1 to insert a node as first node.");
+                Console.WriteLine("Enter 2 to insert a node after a known node.");
+                Console.WriteLine("Enter 3 to insert a node as the last node.");
+                Console.WriteLine("Enter 4 to search for a piece of data.");
+                Console.WriteLine("Enter 5 to delete a node of a given name.");
+                Console.WriteLine(":");
+                Console.WriteLine("Enter any other number to exit.\n\n");
+
+                x = int.Parse(Console.ReadLine());
+                if (x == 0)
+                {
+                    Console.WriteLine("Display the singly Linked list is:");
+                    for (PersonCell cell = TopCell; cell != null; cell = cell.Next)
+                    {
+                        Console.WriteLine(cell.Name);
+                    }
+                }
+                else if (x == 1)
+                {
+                    Console.Write("Enter a name of the new node: ");
+                    string NewName = Console.ReadLine();
+
+                    TopCell.InsertAfter(NewName);
+                }
+                else if (x == 2)
+                {
+
+                    Console.Write("Enter a name of the new node: ");
+                    string NewName = Console.ReadLine();
+                    Console.Write("Enter name of node to insert after it: ");
+                    string AfterName = Console.ReadLine();
+                    //TopCell.InsertAfter(AfterName, NewName);
+
+
+                }
+                else if (x == 3)
+                {
+                    Console.Write("Enter a name of the new node: ");
+                    string NewName = Console.ReadLine();
+                    //TopCell.InsertLast(NewName);
+
+
+                }
+                else if (x == 4)
+                {
+
+                }
+                else if (x == 5)
+                {
+
+                }
+                else break;
+                Console.WriteLine("Press space bar to continue.");
+                Console.ReadKey();
             }
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
